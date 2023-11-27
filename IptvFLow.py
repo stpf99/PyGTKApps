3e3352ae4f7afccda10aa7f19b47e16c4370e0f7
Tomasz Świątek <57900186+stpf99@users.noreply.github.com>
2023-11-27 17:06:12 +0100

    Add files via upload

---
+++ IptvFLow.py
@@ -0,0 +1,428 @@
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('Gst', '1.0')
+gi.require_version('Gdk', '3.0')
+import subprocess
+import requests
+import re
+import os
+from gi.repository import Gtk, GdkPixbuf, Gdk, GObject, GLib, Gst
+import notify2
+import time
+import configparser
+from PIL import Image
+from io import BytesIO
+
+class IPTVPlayer(Gtk.Window):
+    def __init__(self):
+        print("Initializing IPTVPlayer...")
+        # ... (rest of your initialization code)
+
+        super().__init__(title="IPTV Player")
+        self.set_default_size(800, 600)
+
+        # Initialize GStreamer
+        Gst.init(None)
+
+        self.app_dir = "/tmp/iptv_player"
+        os.makedirs(self.app_dir, exist_ok=True)
+        self.destination_dir = ""
+
+        self.play_button = Gtk.Button(label="Play")
+        self.stop_button = Gtk.Button(label="Stop")
+        self.mute_button = Gtk.ToggleButton(label="Mute")
+        self.filechooser_button = Gtk.Button(label="Select Playlist")
+        self.url_entry = Gtk.Entry()
+        self.load_url_button = Gtk.Button(label="Load from URL")
+        self.clear_playlist_button = Gtk.Button(label="Clear Playlist")
+
+        self.url_entry.set_text("http://192.168.1.133:9981/playlist/channels.m3u")
+
+        self.notebook = Gtk.Notebook()
+
+        self.play_button.connect("clicked", self.on_play_button_clicked)
+        self.stop_button.connect("clicked", self.on_stop_button_clicked)
+        self.mute_button.connect("clicked", self.on_mute_button_clicked)
+        self.filechooser_button.connect("clicked", self.on_filechooser_button_clicked)
+        self.load_url_button.connect("clicked", self.on_load_url_button_clicked)
+        self.clear_playlist_button.connect("clicked", self.on_clear_playlist_button_clicked)
+
+        self.status_label = Gtk.Label(label="")
+
+        self.copy_selected_url_button = Gtk.Button(label="Copy Selected URL")
+        self.copy_selected_url_button.connect("clicked", self.on_copy_selected_url_button_clicked)
+
+        # Create a ListStore model for the playlist
+        self.playlist_model = Gtk.ListStore(str, str, str)
+
+        # Create a TreeView with columns
+        playlist_columns = ["Channel Name", "URI", "Logo URL"]
+        self.playlist_treeview = Gtk.TreeView(model=self.playlist_model)
+        for i, column_title in enumerate(playlist_columns):
+            renderer = Gtk.CellRendererText()
+            column = Gtk.TreeViewColumn(column_title, renderer, text=i)
+            self.playlist_treeview.append_column(column)
+
+        # Add the TreeView to a ScrolledWindow
+        scrolled_window = Gtk.ScrolledWindow()
+        scrolled_window.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+        scrolled_window.add(self.playlist_treeview)
+
+        # Connect row-activated signal to play the selected item
+        self.playlist_treeview.connect("row-activated", self.on_playlist_row_activated)
+
+        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.box.set_size_request(800, 700)  # Set a fixed height of 300 pixels (adjust as needed)
+        self.box.set_vexpand(False)
+        self.box.set_hexpand(False)
+        self.box.set_valign(Gtk.Align.START)  # Align to the top
+
+        button_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+
+        button_box.pack_start(self.filechooser_button, False, False, 0)
+        button_box.pack_start(self.url_entry, True, True, 0)
+        button_box.pack_start(self.load_url_button, False, False, 0)
+        button_box.pack_start(self.play_button, False, False, 0)
+        button_box.pack_start(self.stop_button, False, False, 0)
+        button_box.pack_start(self.mute_button, False, False, 0)
+        button_box.pack_start(self.status_label, False, False, 0)
+        button_box.pack_start(self.clear_playlist_button, False, False, 0)
+        button_box.pack_start(self.copy_selected_url_button, False, False, 0)
+
+        self.box.pack_start(button_box, False, False, 0)
+        self.box.pack_start(scrolled_window, True, True, 0)
+
+        self.close_button = Gtk.Button(label="Zamknij program")
+        self.close_button.connect("clicked", self.on_close_button_clicked)
+        self.box.pack_start(self.close_button, False, False, 0)
+
+        self.add(self.box)
+
+        self.player = Gst.ElementFactory.make("playbin", "player")
+        self.bus = self.player.get_bus()
+        self.bus.add_signal_watch()
+        self.bus.connect("message", self.on_bus_message)
+
+        self.notification = notify2.init("IPTV Player")
+        self.stream_start_time = None
+        self.app_start_time = time.time()
+        self.current_channel = ""
+
+        self.mute_button.connect("clicked", self.on_mute_button_clicked)
+        self.config_file_path = os.path.join(self.app_dir, "config.ini")
+        self.config = configparser.ConfigParser()
+        if os.path.exists(self.config_file_path):
+            self.config.read(self.config_file_path)
+
+        self.load_saved_playlists()
+
+        # Additional initialization for CoverFlow
+        self.scrolled_window = Gtk.ScrolledWindow()
+        self.scrolled_window.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+        self.scrolled_window.set_size_request(800, 100)
+        # Use Gtk.Grid instead of Gtk.HBox
+        self.grid = Gtk.Grid()
+        self.grid.set_row_spacing(1)
+        self.grid.set_column_spacing(1)
+        self.scrolled_window.add(self.grid)
+
+        playlist_url = "http://192.168.1.133:9981/playlist/channels.m3u"
+        playlist_content = self.fetch_playlist(playlist_url)
+        image_info = self.extract_image_info(playlist_content)
+
+        self.pixbufs = [self.load_image_from_url(info['url'], info['name'], info['stream_url']) for info in image_info]
+        self.current_index = 0
+        self.previous_stream_pid = None
+
+        self.create_images()
+
+        self.connect("key-press-event", self.on_key_press)
+        print("IPTVPlayer initialization complete.")
+
+    def fetch_playlist(self, url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else ""
+
+    def extract_image_info(self, playlist_content):
+        image_info = []
+        regex_name = re.compile(r'#EXTINF:[^,]*,(.+)\s*$', re.MULTILINE)
+        names = [match.group(1) for match in regex_name.finditer(playlist_content)]
+
+        regex_info = re.compile(r'logo="(.+)",\s*(.+)\s*\n(.+)$', re.MULTILINE)
+        matches = regex_info.finditer(playlist_content)
+
+        for match, name in zip(matches, names):
+            logo_url = match.group(1)
+            stream_url = match.group(3)
+            image_info.append({'url': logo_url, 'name': name, 'stream_url': stream_url})
+
+        return image_info
+
+    def load_image_from_url(self, url, name, stream_url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.content
+            loader = GdkPixbuf.PixbufLoader.new_with_type("png")
+            loader.write(data)
+            loader.close()
+
+            pixbuf = loader.get_pixbuf()
+            if pixbuf is not None:
+                scaled_pixbuf = pixbuf.scale_simple(
+                    80, 40, GdkPixbuf.InterpType.BILINEAR
+                )
+
+                image_with_label = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
+                image = Gtk.Image.new_from_pixbuf(scaled_pixbuf)
+                label = Gtk.Label(label=name)
+                image_with_label.pack_start(image, True, True, 0)
+                image_with_label.pack_start(label, False, False, 0)
+
+                # Save the stream URL
+                image_with_label.stream_url = stream_url
+
+                return image_with_label
+            else:
+                return GdkPixbuf.Pixbuf.new(GdkPixbuf.Colorspace.RGB, True, 8, 1, 1).fill(0xFFFFFFFF)
+        else:
+            return GdkPixbuf.Pixbuf.new(GdkPixbuf.Colorspace.RGB, True, 8, 1, 1).fill(0xFFFFFFFF)
+
+    def on_key_press(self, widget, event):
+        keyval = event.keyval
+
+        if keyval == Gdk.KEY_Left:
+            self.show_previous_image()
+        elif keyval == Gdk.KEY_Right:
+            self.show_next_image()
+        elif keyval == Gdk.KEY_Return:
+            self.play_stream()
+
+    def play_stream(self):
+        if 0 <= self.current_index < len(self.pixbufs) and self.pixbufs[self.current_index] is not None:
+            stream_url = self.pixbufs[self.current_index].stream_url
+            if stream_url:
+                print(f"Playing stream: {stream_url}")
+
+                # Stop the previous playback
+                self.stop_previous_stream()
+
+                # Start the new playback
+                process = subprocess.Popen(["gst-launch-1.0", "playbin", "uri=" + stream_url])
+                self.previous_stream_pid = process.pid
+                self.update_images()
+
+    def stop_previous_stream(self):
+        # Check if there is a previous GStreamer process
+        if self.previous_stream_pid is not None:
+            try:
+                # Terminate the previous GStreamer process
+                subprocess.run(["kill", str(self.previous_stream_pid)], check=True)
+            except subprocess.CalledProcessError:
+                print(f"Error stopping the previous GStreamer process (PID: {self.previous_stream_pid})")
+
+    def show_previous_image(self):
+        if self.current_index > 0:
+            self.current_index -= 1
+            self.update_images()
+
+    def show_next_image(self):
+        if self.current_index < len(self.pixbufs) - 1:
+            self.current_index += 1
+            self.update_images()
+
+    def update_images(self):
+        for child in self.box.get_children():
+            self.box.remove(child)
+
+        self.create_images()
+
+    def create_images(self):
+        middle_index = self.current_index + 0
+
+        for i in range(middle_index - 0, middle_index + 1):
+            if 0 <= i < len(self.pixbufs) and self.pixbufs[i] is not None:
+                if i == middle_index:
+                    scaled_pixbuf = self.pixbufs[i].get_children()[0].get_pixbuf().scale_simple(
+                        int(self.pixbufs[i].get_children()[0].get_pixbuf().get_width() * 2),
+                        int(self.pixbufs[i].get_children()[0].get_pixbuf().get_height() * 2),
+                        GdkPixbuf.InterpType.BILINEAR
+                    )
+                    image = Gtk.Image.new_from_pixbuf(scaled_pixbuf)
+                else:
+                    image = Gtk.Image.new_from_pixbuf(self.pixbufs[i].get_children()[0].get_pixbuf())
+            else:
+                image = Gtk.Image()
+                image.set_can_focus(False)
+            self.box.pack_start(image, True, True, 0)
+
+        if 0 <= self.current_index < len(self.pixbufs) and self.pixbufs[self.current_index] is not None:
+            self.scrolled_window.get_hadjustment().set_value(self.pixbufs[self.current_index].get_children()[0].get_pixbuf().get_width() * (middle_index - 4))
+        self.show_all()
+
+    def on_copy_selected_url_button_clicked(self, widget):
+        selection = self.playlist_treeview.get_selection()
+        model, selected_iter = selection.get_selected()
+        if selected_iter:
+            playlist_url = model.get_value(selected_iter, 1)
+            clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+            clipboard.set_text(playlist_url, -1)
+            clipboard.store()
+            print("Selected Playlist URL copied to clipboard.")
+
+    def on_filechooser_button_clicked(self, widget):
+        dialog = Gtk.FileChooserDialog(
+            title="Select Playlist", parent=self, action=Gtk.FileChooserAction.OPEN
+        )
+        dialog.add_buttons(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK)
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            playlist_path = dialog.get_filename()
+            self.load_playlist(playlist_path)
+        dialog.destroy()
+
+    def on_play_button_clicked(self, widget):
+        active_iter = self.playlist_treeview.get_selection().get_selected()
+        if active_iter:
+            uri = self.playlist_model.get_value(active_iter[1], 1)
+            self.player.set_property("uri", uri)
+            self.player.set_state(Gst.State.PLAYING)
+            self.stream_start_time = time.time()
+            self.current_channel = self.playlist_model.get_value(active_iter[1], 0)
+            self.status_label.set_label(f"Playing: {self.current_channel}")
+            self.show_now_playing_notification()
+
+    def on_stop_button_clicked(self, widget):
+        self.player.set_state(Gst.State.NULL)
+        self.stream_start_time = None
+        self.status_label.set_label("Stopped")
+
+    def on_mute_button_clicked(self, widget):
+        new_mute_state = widget.get_active()
+        self.player.set_property("mute", new_mute_state)
+
+    def on_bus_message(self, bus, message):
+        if message.type == Gst.MessageType.EOS:
+            self.player.set_state(Gst.State.NULL)
+            self.stream_start_time = None
+            self.status_label.set_label("End of stream")
+
+    def show_now_playing_notification(self):
+        current_time = time.strftime("%H:%M:%S")
+        notification_text = f"Now Playing: {self.current_channel}\nCurrent Time: {current_time}"
+
+        # Add this part to include the logo in the notification
+        logo_url = self.get_logo_url_for_channel(self.current_channel)
+        if logo_url:
+            logo_data = requests.get(logo_url).content
+            logo_image = Image.open(BytesIO(logo_data))
+
+            # Adjust the size as needed
+            logo_image = logo_image.resize((400, 200))
+
+            logo_image_path = "/tmp/logo_image.png"  # Temporary path to save the resized image
+            logo_image.save(logo_image_path)
+
+            # Display the notification with the logo
+            notification = notify2.Notification("IPTV Player", notification_text, icon=logo_image_path)
+        else:
+            # Display the notification without the logo if the URL is not available
+            notification = notify2.Notification("IPTV Player", notification_text)
+
+        notification.show()
+
+    def get_logo_url_for_channel(self, channel_name):
+        # Implement logic to get the logo URL for the given channel
+        # This could involve searching your playlist model or using a predefined mapping
+        # Replace this with your actual logic based on your playlist structure
+        for row in self.playlist_model:
+            if row[0] == channel_name:
+                return row[2]  # Assuming the logo URL is in the third column (adjust as needed)
+        return None  # Return None if the logo URL is not found
+
+    def load_playlist(self, playlist_path):
+        self.playlist_model.clear()  # Clear the existing playlist
+        if os.path.exists(playlist_path):
+            with open(playlist_path, "r") as f:
+                lines = f.readlines()
+                channel_name = ""
+                logo_url = None
+                for i, line in enumerate(lines):
+                    if line.startswith("#EXTINF"):
+                        channel_name = line.split(",", 1)[-1].strip()
+                        # Extracting logo_url if available
+                        if "logo=" in line:
+                            logo_url = line.split("logo=")[-1].split('"')[1]
+                    elif line.startswith("http"):
+                        # Adding the entry to the playlist model
+                        # If logo_url is None, provide an empty string as a placeholder
+                        self.playlist_model.append([channel_name, line.strip(), logo_url or ""])
+                        # Resetting logo_url for the next entry
+                        logo_url = None
+
+    def on_close_button_clicked(self, widget):
+        self.save_playlists()
+        Gtk.main_quit()
+
+    def on_playlist_row_activated(self, treeview, path, column):
+        self.on_play_button_clicked(self.play_button)
+
+    def on_load_url_button_clicked(self, widget):
+        playlist_url = self.url_entry.get_text()
+        if playlist_url.startswith("http"):
+            response = requests.get(playlist_url)
+            if response.status_code == 200:
+                playlist_content = response.text
+                self.load_playlist_from_content(playlist_content)
+            else:
+                print(f"Failed to fetch playlist from URL. Status code: {response.status_code}")
+        else:
+            print("Invalid URL format")
+
+    def on_clear_playlist_button_clicked(self, widget):
+        self.playlist_model.clear()
+
+    def save_playlists(self):
+        config = configparser.ConfigParser()
+        config.add_section('Playlists')
+        for index, playlist in enumerate(self.get_playlist_entries()):
+            config.set('Playlists', f'Playlist{index}', playlist)
+
+        with open(self.config_file_path, 'w') as configfile:
+            config.write(configfile)
+
+    def load_saved_playlists(self):
+        if os.path.exists(self.config_file_path):
+            config = configparser.ConfigParser()
+            config.read(self.config_file_path)
+
+            if 'Playlists' in config:
+                for key, value in config.items('Playlists'):
+                    self.load_playlist(value)
+
+    def get_playlist_entries(self):
+        entries = []
+        for row in self.playlist_model:
+            entries.append(row[1])
+        return entries
+
+    def load_playlist_from_content(self, playlist_content):
+        self.playlist_model.clear()  # Clear the existing playlist
+        lines = playlist_content.splitlines()
+        channel_name = ""
+        logo_url = None
+        for i, line in enumerate(lines):
+            if line.startswith("#EXTINF"):
+                channel_name = line.split(",", 1)[-1].strip()
+                # Extracting logo_url if available
+                if "logo=" in line:
+                    logo_url = line.split("logo=")[-1].split('"')[1]
+            elif line.startswith("http"):
+                # Adding the entry to the playlist model
+                # If logo_url is None, provide an empty string as a placeholder
+                self.playlist_model.append([channel_name, line.strip(), logo_url or ""])
+                # Resetting logo_url for the next entry
+                logo_url = None
+
+if __name__ == "__main__":
+    player = IPTVPlayer()
+    Gtk.main()
